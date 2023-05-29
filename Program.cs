c2b6757c9b94c85aaed3907d1194a2bbf51909b1
Nikita Sychev <nikitasychev@inbox.ru>
2023-05-29 20:34:58 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extension_method_homeWork
+{
+    public static class StringExtension {  // класс расширения
+     public static string CaesarEncrypt(this string input, int shift)
+    {
+        string result = "";
+        foreach (char c in input.ToUpper())
+        {
+                if (c >= 'A' || c <= 'Z')
+                {
+                    char newChar = (char)(((int)c + shift - 65) % 26 + 65);
+                    result += newChar;
+                }
+                else if (c >= 'a' || c <= 'z')
+                {
+
+                }
+        }
+        return result;
+    }
+
+    public static string CaesarDecrypt(this string input, int shift)
+    {
+        string result = "";
+        foreach (char c in input.ToUpper())
+        {
+                char newChar = (char)(((int)c - shift - 65 + 26) % 26 + 65);
+                result += newChar;          
+        }
+        return result;
+    }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово для шифра");
+            string word = Console.ReadLine(); // считали строку
+            Console.WriteLine($"Слово для шифра: {word}");
+
+            int shiftNumber;
+            do
+            {
+                Console.WriteLine("Введите цифра для шифрования от 1 до 26");
+                shiftNumber = int.Parse(Console.ReadLine()); // считали число
+                Console.WriteLine($"Вы ввели число {shiftNumber}");
+            } while (shiftNumber < 1 || shiftNumber > 26 ); // зациклили ввод
+
+            Console.WriteLine($"Слово с зашифровкой - {word.CaesarEncrypt(shiftNumber)}"); // сделали вывод 
+            string newWord = word.CaesarEncrypt(shiftNumber);
+            Console.WriteLine($"Слово с расшифровкой - {newWord.CaesarDecrypt(shiftNumber)}");
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
